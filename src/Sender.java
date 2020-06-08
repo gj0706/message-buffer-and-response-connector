c923655194170cc15f7f61d36f9d7b34c3d307c3
Jian Guo <guojian0706@gmail.com>
2020-06-08 17:19:06 -0500

    Initial commit

---
+++ src/Sender.java
@@ -0,0 +1,13 @@
+public class Sender {
+
+    public void printMessage(String msg){
+        System.out.println(msg);
+    }
+    public void printReceivedResult(String result){
+        System.out.println(result);
+    }
+
+
+
+
+}
